c2916371034648806583834d6dfc2c43d18bff5b
VishalPatel43 <202211012@daiict.ac.in>
2024-07-31 12:13:36 +0530

    create the bean of  Apple class, make Component, and use the @Autowire

--- IntroductionToSpringBoot/src/main/java/com/springboot/coding/introductionToSpringBoot/Apple.java
+++ IntroductionToSpringBoot/src/main/java/com/springboot/coding/introductionToSpringBoot/Apple.java
@@ -1,5 +1,9 @@
 package com.springboot.coding.introductionToSpringBoot;
 
+import org.springframework.stereotype.Component;
+
+@Component // this annotation is used to create the object of the class,
+// notified to the spring that this is the class which is going to be used as a bean
 public class Apple {
 
     void eatApple() {
--- IntroductionToSpringBoot/src/main/java/com/springboot/coding/introductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ IntroductionToSpringBoot/src/main/java/com/springboot/coding/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,17 +1,27 @@
 package com.springboot.coding.introductionToSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
+
+    @Autowired // this annotation is used to inject the object of the class
+    // inject the dependency of Apple inside Spring boot Bean
+    // field injection
+    Apple obj;
 
     public static void main(String[] args) {
-
-        Apple apple = new Apple();
-        apple.eatApple();
 
         SpringApplication.run(IntroductionToSpringBootApplication.class, args);
     }
 
+    // non-static method we can use the coz we don't want to create the object of the class
+    @Override
+    public void run(String... args) throws Exception {
+        obj.eatApple();
+
+    }
 }
