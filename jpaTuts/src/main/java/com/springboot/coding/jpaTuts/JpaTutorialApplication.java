42cb64023682a86dd3672f3837edf703c82fb7b3
VishalPatel43 <202211012@daiict.ac.in>
2024-08-12 13:03:04 +0530

    JPA Tutorial

---
+++ jpaTuts/src/main/java/com/springboot/coding/jpaTuts/JpaTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.coding.jpaTuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTutorialApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Spring Boot!");
+		SpringApplication.run(JpaTutorialApplication.class, args);
+	}
+
+}
---
+++ jpaTuts/src/test/java/com/springboot/coding/jpaTuts/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.coding.jpaTuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
