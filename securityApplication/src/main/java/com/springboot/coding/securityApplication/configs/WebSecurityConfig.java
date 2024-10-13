4840baf81ce8ca4d55b87c5cb70feb5d2b716bc0
VishalPatel43 <202211012@daiict.ac.in>
2024-10-13 14:20:25 +0530

    fix the password encoder method for inMemoryUserDetails

--- securityApplication/src/main/java/com/springboot/coding/securityApplication/configs/WebSecurityConfig.java
+++ securityApplication/src/main/java/com/springboot/coding/securityApplication/configs/WebSecurityConfig.java
@@ -3,12 +3,10 @@ package com.springboot.coding.securityApplication.configs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -17,13 +15,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity // Enable Spring Security and configure security filter chain
@@ -38,9 +29,8 @@ public class WebSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity
-//                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF as we are using JWT
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF as we are using JWT
                 .cors(AbstractHttpConfigurer::disable) // Disable CORS at security level, managed in WebConfig
-
 //                .sessionManagement(session -> session
 //                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless sessions with JWT
 //                )
@@ -65,17 +55,15 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    UserDetailsService myInMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
+    UserDetailsService myInMemoryUserDetailsManager() {
         UserDetails normalUser = User
                 .withUsername("user")
-//                .password("{bcrypt}$2a$10$sTH.vPQFLXD1CS7NGdR6W.LBBFy7fbu5nCXXb/.FOIH0mgJ3mq5YG")
                 .password(passwordEncoder.encode("1234"))
                 .roles("USER")
                 .build();
 
         UserDetails adminUser = User
                 .withUsername("ADMIN")
-//                .password("{bcrypt}$2a$10$0beBvHgkgZ2lt1S9tTqAZ.Vta.iuth22t3hKPwrb7GYagocPDJbKu")
                 .password(passwordEncoder.encode("admin"))
                 .roles("ADMIN")
                 .build();
